abddaf82834231fb3d02c18e957e520849c8d753
noaner <noahbweiner@gmail.com>
2017-12-12 18:27:52 -0800

    set up github pages

--- webpack/config-helper.js
+++ webpack/config-helper.js
@@ -2,62 +2,60 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = options => ({
-  entry: ['./src/index.js'],
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify(
-          options.production ? 'production' : 'development'
-        )
-      }
-    }),
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      minify: options.production && {
-        collapseWhitespace: true,
-        conservativeCollapse: true,
-        decodeEntities: true,
-        minifyCSS: true,
-        minifyJS: true,
-        removeComments: true
-      }
-    }),
-    ...(options.production
-      ? [
-        webpack.optimize.UglifyJsPlugin({
-          compressor: {
-            warnings: false
-          }
-        })
-      ]
-      : [])
-  ],
-  devtool: options.production ? 'source-map' : 'cheap-eval-source-map',
-  devServer: !options.production && {
-    contentBase: './dist',
-    inline: true
-  },
-  output: {
-    filename: '[name].[chunkhash].js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['es2017']
-          }
+module.exports = options => {
+  const config = {
+    entry: ['./src/index.js'],
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env': {
+          NODE_ENV: JSON.stringify(
+            options.production ? 'production' : 'development'
+          )
         }
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
+      }),
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        minify: options.production && {
+          collapseWhitespace: true,
+          conservativeCollapse: true,
+          decodeEntities: true,
+          minifyCSS: true,
+          minifyJS: true,
+          removeComments: true
+        }
+      })
+    ],
+    devtool: options.production ? 'source-map' : 'cheap-eval-source-map',
+    output: {
+      filename: '[name].[chunkhash].js',
+      path: path.resolve(__dirname, '..', 'dist')
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['es2017']
+            }
+          }
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
+        }
+      ]
+    }
+  };
+
+  if (!options.production) {
+    config.devServer = {
+      contentBase: './dist',
+      inline: true
+    };
   }
-});
+
+  return config;
+};
